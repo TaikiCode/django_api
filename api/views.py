6c57a4399647655e0051dcddb02d322d00b600ee
TaikiCode <metoro5151@gmail.com>
2021-02-16 14:48:17 +0900

    create api-endpoint

---
+++ api/views.py
@@ -0,0 +1,45 @@
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from rest_framework import viewsets
+from .serializers import TaskSerializer, UserSerializer, PostSerializer
+from .models import Task, Post
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+    
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
+
+# 特定のデータ
+class PostRetrieveView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
+    
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (AllowAny,)
+    
+class TaskRetrieveView(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (AllowAny,)
+    
+class TaskViewSet(viewsets.ModelViewSet):
+    '''
+    新規作成、編集、削除
+    '''
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
+    
+    
+
+
